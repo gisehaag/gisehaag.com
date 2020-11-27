47885e6a50aed0eaa8f70b42c408c3d10eef7457
Gise Haag <ing.giselahaag@gmail.com>
2020-11-28 00:28:25 +0100

    update files of theme Gisehaag

---
+++ gisehaag/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
+</head>
+
+<body>
+	<header class="header-bgc">
+		<div class="header-container box-width">
+			<div class="main-menu">
+					<ul>
+						<li><a href="#">HOME</a></li>
+						<li><a href="#">BLOG</a></li>
+						<li><a href="#">SOBRE MI</a></li>
+						<li><a href="#">CONTACTAME</a></li>
+					</ul>
+			</div>
+			<div class="social-menu">
+				<ul>
+					<li><a href="https://www.instagram.com/gise_h/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/gisela-haag-a0504330/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="https://github.com/gisehaag" target="_blank"><i class="fab fa-github"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
---
+++ gisehaag/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+	<section class="logo-container">
+		<div class="site-title">
+			<h1 class="title-logo">gisehaag</h1>
+			<p class="subtitle-logo">__un subtitulo muy copado va acá__</p>
+		</div>
+		<div class="box-width img-bgc">
+			<div class="main-img">
+				<img  src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/e35/47585739_2137268623253414_1639102644497674294_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=McB2SueSkVUAX8bi-jK&tp=1&oh=0f36d0fd9c3b431cb76881e62ef08290&oe=5FE99DC4" alt="">
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="box-width img-bgc">
+
+
+
+		</div>
+	</section>
+
+<?php get_footer('page'); ?>
