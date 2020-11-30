c4b594a5e0c64b727743750159c6f661137ad4c3
Gise Haag <ing.giselahaag@gmail.com>
2020-11-30 18:16:19 +0100

    change home content

--- gisehaag/index.php
+++ gisehaag/index.php
@@ -2,7 +2,7 @@
 	<section>
 		<div class="site-title">
 			<h1 class="title-logo">gisehaag</h1>
-			<p class="subtitle-logo">__un subtitulo muy copado va acá__</p>
+			<p class="subtitle-logo uppercase-text">__un subtitulo muy copado va acá__</p>
 		</div>
 		<div class="box-width img-bgc">
 			<div class="main-img">
@@ -11,11 +11,59 @@
 		</div>
 	</section>
 	<section>
-		<div class="box-width img-bgc">
+		<div class="flex box-width">
+			<a class="link-box" href="#">
+				<div class="box-image">
+					<img src="https://cdn.pixabay.com/photo/2015/09/20/02/03/sunglasses-947892_1280.jpg" alt="">
+					<h2 class="uppercase-text">blog</h2>
+				</div>
+			</a>
 
-
-
+			<a class="link-box" href="#">
+				<div class="box-image">
+					<img src="https://cdn.pixabay.com/photo/2017/02/15/16/16/converse-2069209_1280.jpg" alt="">
+					<h2 class="uppercase-text">sobre mi</h2>
+				</div>
+			</a>
+			<a class="link-box" href="#">
+				<div class="box-image">
+					<img src="https://cdn.pixabay.com/photo/2015/04/24/18/04/stamps-738059_1280.jpg" alt="">
+					<h2 class="uppercase-text">contactame</h2>
+				</div>
+			</a>
 		</div>
+	</section>
+	<section>
+		<div class="flex box-width ">
+			<div class="blog-article white-box">
+				<div>
+					<p class="post-category uppercase-text">
+						<a href="#">lifestyle</a>
+					</p>
+					<h1 class="post-title uppercase-text">
+						There is so much pleasure in reading
+					</h1>
+					<p class="post-meta">
+					 <span><i class="fas fa-user"></i> Daisy</span><span id="separator">/</span><span><i class="fas fa-calendar-day"></i> September 8, 2018</span>
+					</p>
+					<img class="post-image" src="https://daisy.aqthemes.com/wp-content/uploads/2017/09/blog11.jpg" alt="">
+					<div class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit scelerisque porttitor. Nunc hendrerit ipsum leo. In maximus maximus purus et porttitor. Sed placerat elementum urna ac ullamcorper. In nulla elit, vestibulum quis metus ut, dictum volutpat enim. Ut non diam sed nulla fringilla ornare. Mauris et venenatis leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis scelerisque consectetur orci, sit amet convallis turpis ullamcorper non. Cras dignissim orci ut nibh pretium, a porttitor mauris interdum. Donec tellus nisi, ornare egestas rhoncus non, interdum id urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec molestie est id turpis vestibulum, id fermentum risus sagittis.
+					</div>
+				</div>
+			</div>
+			<div class="box-about-me white-box">
+				<h1 class="about-me-title uppercase-text">
+					sobre mi
+				</h1>
+				<img src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/s320x320/27579869_181277045979607_1174785349159747584_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_ohc=IIWDpOZNe8wAX-1z47M&tp=1&oh=1581982b7cae101d4f1fea9f6b3a18d7&oe=5FEFB393" alt="" class="avatar">
+				<div class="bio">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit scelerisque porttitor. Nunc hendrerit ipsum leo.
+				</div>
+			</div>
+		</div>
+
+
 	</section>
 
 <?php get_footer('page'); ?>
