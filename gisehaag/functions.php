be0ae022d54d97ddc1ba58f1f8e100831be294e9
Gise Haag <ing.giselahaag@gmail.com>
2020-11-30 18:15:02 +0100

    add google font monserrat

--- gisehaag/functions.php
+++ gisehaag/functions.php
@@ -16,12 +16,14 @@ function assets(){
 
    wp_register_style('lato', 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap', '1.0', 'all');
 
+   wp_register_style('monserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', '1.0', 'all');
+
    wp_register_style('charmonman', 'https://fonts.googleapis.com/css2?family=Charmonman:wght@400;700&display=swap', '1.0', 'all');
 
    wp_register_style('fontawsome', 'https://use.fontawesome.com/releases/v5.15.1/css/all.css', '5.15.1', 'all');
 
 
-   wp_enqueue_style('estilos', get_stylesheet_uri(), array('lato', 'charmonman', 'fontawsome'), '1.0', 'all');
+   wp_enqueue_style('estilos', get_stylesheet_uri(), array('lato', 'monserrat', 'charmonman', 'fontawsome'), '1.0', 'all');
 
    wp_enqueue_script( 'custom', get_template_directory_uri().'/assets/js/custom.js', '', '1.0', true);
 
