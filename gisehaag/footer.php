76291b396eea2b757263c60e944c957ff4dcada0
Gise Haag <ing.giselahaag@gmail.com>
2020-12-01 13:04:49 +0100

    Add content footer

--- gisehaag/footer.php
+++ gisehaag/footer.php
@@ -1,3 +1,11 @@
+<footer class="header-footer-bgc">
+	<div class="footer-content">
+		<i>✏</i> Developed with<i>❤</i>by <a href="gisehaag.con">GisehaaG</a><i>⭐</i> |<i></i> 2020
+	</div>
+
+</footer>
+
+
 <?php wp_footer(); ?>
 
 </body>
