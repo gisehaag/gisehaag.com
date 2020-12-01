5655944cba5cf37016f4515bddedc8efcf369889
Gise Haag <ing.giselahaag@gmail.com>
2020-12-01 13:05:17 +0100

    Add media queries

--- gisehaag/index.php
+++ gisehaag/index.php
@@ -34,7 +34,7 @@
 		</div>
 	</section>
 	<section>
-		<div class="flex box-width ">
+		<div class="flex box-width post-section">
 			<div class="blog-article white-box">
 				<div>
 					<p class="post-category uppercase-text">
@@ -56,9 +56,11 @@
 				<h1 class="about-me-title uppercase-text">
 					sobre mi
 				</h1>
-				<img src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/s320x320/27579869_181277045979607_1174785349159747584_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_ohc=IIWDpOZNe8wAX-1z47M&tp=1&oh=1581982b7cae101d4f1fea9f6b3a18d7&oe=5FEFB393" alt="" class="avatar">
-				<div class="bio">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit scelerisque porttitor. Nunc hendrerit ipsum leo.
+				<div class="bio-wrapper">
+					<img  class="avatar" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/s320x320/27579869_181277045979607_1174785349159747584_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_ohc=IIWDpOZNe8wAX-1z47M&tp=1&oh=1581982b7cae101d4f1fea9f6b3a18d7&oe=5FEFB393" alt="">
+					<div class="bio">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit scelerisque porttitor. Nunc hendrerit ipsum leo.
+					</div>
 				</div>
 			</div>
 		</div>
