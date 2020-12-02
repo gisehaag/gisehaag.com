c9056e96816bcff4044dd6422ae6a110d4df3723
Gise Haag <ing.giselahaag@gmail.com>
2020-12-02 11:13:23 +0100

    Add contact form

--- gisehaag/footer.php
+++ gisehaag/footer.php
@@ -1,3 +1,11 @@
+<div class="box-width">
+	<div class="contact-me white-box" id="contact-me">
+		<h2 class="post-title uppercase-text">ponete en contacto conmigo completando este formulario 😊</h2>
+		<?php echo do_shortcode('[contact-form-7 id="25" title="Contact form 1"]');?>
+	</div>
+</div>
+
+
 <footer class="header-footer-bgc">
 	<div class="footer-content">
 		<i>✏</i> Developed with<i>❤</i>by <a href="gisehaag.con">GisehaaG</a><i>⭐</i> |<i></i> 2020
