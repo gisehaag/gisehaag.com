d49d033b32aeeb200c936fe9a7f89ab41c901ada
Gise Haag <ing.giselahaag@gmail.com>
2020-12-03 13:17:51 +0100

    Create sidebar

---
+++ gisehaag/sidebar.php
@@ -0,0 +1,11 @@
+<div class="box-about-me white-box" id="about-me">
+	<h1 class="about-me-title uppercase-text">
+		sobre mi
+	</h1>
+	<div class="bio-wrapper">
+		<img  class="avatar" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/s320x320/27579869_181277045979607_1174785349159747584_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_ohc=IIWDpOZNe8wAX-1z47M&tp=1&oh=1581982b7cae101d4f1fea9f6b3a18d7&oe=5FEFB393" alt="">
+		<div class="bio">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit scelerisque porttitor. Nunc hendrerit ipsum leo.
+		</div>
+	</div>
+</div>
