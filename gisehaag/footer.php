59dd00a2b141ab79bd37630effd2903924f42499
Gise Haag <ing.giselahaag@gmail.com>
2020-12-08 16:25:17 +0100

    Fix home link

--- gisehaag/footer.php
+++ gisehaag/footer.php
@@ -1,6 +1,6 @@
 <footer class="header-footer-bgc">
 	<div class="footer-content">
-		<i>✏</i> Developed with<i>❤</i>by <a href="gisehaag.con">GisehaaG</a><i>⭐</i> |<i></i> 2020
+		<i>✏</i> Developed with<i>❤</i>by <a href="<?php echo home_url(); ?>">GisehaaG</a><i>⭐</i> |<i></i> 2020
 	</div>
 
 </footer>
