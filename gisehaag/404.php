3dabf342bdc4b78f4d1c81a36c1645af27777285
Gise Haag <ing.giselahaag@gmail.com>
2020-12-08 16:24:40 +0100

    Create 404.php

---
+++ gisehaag/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<main>
+	<div class="flex box-width post-section white-box">
+		<h1 class="title uppercase-text monserrat-font">
+			La página no existe, volvé haciendo
+			<a href="<?php echo home_url(); ?>">click acá 😉</a>
+		</h1>
+	</div>
+</main>
+
+<?php get_footer(); ?>
