f35383894cab5ae5b69be84597340f0d3195a1d7
Gise Haag <ing.giselahaag@gmail.com>
2021-01-21 14:09:05 +0100

    Fix a typo

--- gisehaag/footer.php
+++ gisehaag/footer.php
@@ -1,7 +1,7 @@
 <footer class="header-footer-bgc">
 	<div class="footer-content">
 		<?php echo sprintf(
-			'<i>✏</i> %s<i>❤</i>%s <a href="%s">%s</a><i>⭐</i> |<i> 2020 - %s',
+			'<i>✏</i> %s<i>❤</i>%s <a href="%s">%s</a><i>⭐</i> |<i> 2020 - %s </i>',
 			__('Developed with', 'gisehaag'),
 			__('by', 'gisehaag'),
 			home_url(),
