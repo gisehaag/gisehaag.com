20d297a16a33582fbb49711d4501a6794cddf879
Gise Haag <ing.giselahaag@gmail.com>
2021-02-19 17:57:10 +0100

    Add projects shortcode

--- gisehaag/inc/shortcodes.php
+++ gisehaag/inc/shortcodes.php
@@ -8,3 +8,11 @@ function gh_latest_post_shortcode() {
 }
 
 add_shortcode( 'latest-post', 'gh_latest_post_shortcode' );
+
+function gh_projects_shortcode() {
+	ob_start();
+	get_template_part( 'template-parts/shortcodes/projects' );
+	return ob_get_clean();
+}
+
+add_shortcode( 'projects', 'gh_projects_shortcode' );
---
+++ gisehaag/template-parts/shortcodes/projects.php
@@ -0,0 +1,33 @@
+<?php
+$args = array(
+	'post_type'      => 'project-item',
+	'posts_per_page' => -1,
+	'order'          => 'ASC',
+	'order_by'       => 'date',
+
+);
+
+$projects = new WP_Query($args); ?>
+
+<section id="projects">
+	<?php if( $projects->have_posts() ): ?>
+		<div class="projects">
+			<?php while ( $projects->have_posts() ) : $projects->the_post(); ?>
+				<div class="project-item" data-name="<?php echo $post->post_name ?>">
+					<a href="<?php echo get_field('project_url'); ?>" target="_blank">
+						<img
+							class="project-img"
+							src="<?php echo get_field('image'); ?>"
+							alt="<?php echo get_field('image_alt'); ?>"
+						/>
+					</a>
+					<h1 class="project-title"> <span class="icon-chevron-down"></span> <?php the_title(); ?></h1>
+					<div class="details-project"></div>
+				</div>
+			<?php endwhile; ?>
+		</div>
+	<?php endif; ?>
+</section>
+
+
+
